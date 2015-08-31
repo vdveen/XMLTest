84f95f145cc8b1c43481264a113790b2c8a45e4c
Anne van der Veen <asvdveen@gmail.com>
2015-08-31 22:05:05 +0200

    Successfully reads out an XML's values

---
+++ XMLParse/XMLParse.py
@@ -0,0 +1,16 @@
+﻿#Goal: 
+#Import XML file 
+#Put info from XML in a neat class with attributes
+
+import xml.etree.ElementTree as ET
+
+
+#The tree is the entire XML tree, and its root is the top level of the tree. In this case, 'stations'. 
+tree = ET.parse('E:\OneDrive\GIS\Expe\Visual Studio App\livecyclehireupdates.xml')
+root = tree.getroot()
+
+#List all the stations
+for station in root:
+    print station[1].tag, station[1].text
+
+
