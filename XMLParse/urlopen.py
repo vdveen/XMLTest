d6cf7a6ef88a6d8d1646d2c8aa87b8f3a31c6391
Anne van der Veen <asvdveen@gmail.com>
2015-09-03 23:12:41 +0200

    Attempted to open URL, will do later because of later switch to Py3

--- XMLParse/urlopen.py
+++ XMLParse/urlopen.py
@@ -1,3 +1,5 @@
 from urllib2 import urlopen
 print 1
 xml = urlopen("https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml")
+
+#Found out that urllib is totally different in Python 3. TODO
